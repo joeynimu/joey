a9fc52c9b45aa4d797fce474d58e6ff0da76208a
Joe Ng'ethe <joeynimu@gmail.com>
2020-04-13 16:27:20 +0300

    chore: gatsby config

---
+++ gatsby-config.js
@@ -0,0 +1,52 @@
+require(`dotenv`).config({
+  path: `.env`
+});
+
+module.exports = {
+  siteMetadata: {
+    siteTitleAlt: `Joey Ng'ethe`
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      options: {
+        navigation: [{}],
+        externalLinks: []
+      }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID
+      }
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Joey Ng'ethe - Javascript Engineer`,
+        short_name: `Joey`,
+        description: `I am a Frontend/Javascript Engineer from Nairobi, Kenya. I love using React, GraphQL, Node and everything in between.`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#6B46C1`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`
+          }
+        ]
+      }
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`
+    // `gatsby-plugin-webpack-bundle-analyser-v2`,
+  ]
+};
