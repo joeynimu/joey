9c9146165f9f00114af213b5b883fdaba2a9710a
Joe Ng'ethe <joeynimu@gmail.com>
2020-04-23 10:55:59 +0300

    feat: updated brand icons i.e favicon, PWA images and theming

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,13 @@ require(`dotenv`).config({
 module.exports = {
   siteMetadata: {
     siteTitle: `Joey Ng'ethe`,
-    siteTitleAlt: `Joey Ng'ethe`
+    siteTitleAlt: `Joey's Blog - Javascript Engineer`,
+    siteHeadline: `Joey's Blog - Javascript Engineer; React, GraphQL, Node and everything in between`,
+    siteUrl: `https://joeynimu.com`,
+    siteLanguage: `en`,
+    siteImage: `/banner.jpg`,
+    siteDescription: `Joey Ng'ethe is a Javascript Engineer from Nairobi, Kenya.`,
+    author: `@africansinatra`
   },
   plugins: [
     {
@@ -26,11 +32,11 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Joey Ng'ethe - Javascript Engineer`,
-        short_name: `Joey`,
+        short_name: `Joey's Blog`,
         description: `I am a Frontend/Javascript Engineer from Nairobi, Kenya. I love using React, GraphQL, Node and everything in between.`,
         start_url: `/`,
         background_color: `#fff`,
-        theme_color: `#6B46C1`,
+        theme_color: `#000`,
         display: `standalone`,
         icons: [
           {
