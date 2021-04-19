bd57ef444ec272c0b68fedaeafd24d9a844be707
Joe Ng'ethe <joeynimu@gmail.com>
2021-04-19 13:18:20 +0300

    new blog post: react-query and graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 require(`dotenv`).config({
-  path: `.env`
+  path: `.env`,
 });
 
 module.exports = {
@@ -11,21 +11,36 @@ module.exports = {
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
     siteDescription: `Joey Ng'ethe is a Javascript Engineer from Nairobi, Kenya.`,
-    author: `@africansinatra`
+    author: `@africansinatra`,
   },
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-minimal-blog`,
       options: {
         navigation: [],
-        externalLinks: []
-      }
+        externalLinks: [],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        // Defaults used for gatsbyImageData and StaticImage
+        defaults: {},
+        // Set to false to allow builds to continue on image errors
+        failOnError: true,
+        // deprecated options and their defaults:
+        base64Width: 20,
+        forceBase64Format: ``, // valid formats: png,jpg,webp
+        useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+        stripMetadata: true,
+        defaultQuality: 50,
+      },
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID
-      }
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
     },
     `gatsby-plugin-sitemap`,
     {
@@ -42,20 +57,20 @@ module.exports = {
           {
             src: `/android-chrome-192x192.png`,
             sizes: `192x192`,
-            type: `image/png`
+            type: `image/png`,
           },
           {
             src: `/android-chrome-512x512.png`,
             sizes: `512x512`,
-            type: `image/png`
-          }
-        ]
-      }
+            type: `image/png`,
+          },
+        ],
+      },
     },
     `gatsby-plugin-twitter`,
     `gatsby-plugin-offline`,
     `gatsby-plugin-netlify`,
     `gatsby-plugin-webpack-bundle-analyser-v2`,
-    `@pauliescanlon/gatsby-mdx-embed`
-  ]
+    `@pauliescanlon/gatsby-mdx-embed`,
+  ],
 };
