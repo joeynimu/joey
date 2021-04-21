27b8d215bf8bf2f0d64a06daf53a005a5b1daf0e
Joe Ng'ethe <joeynimu@gmail.com>
2021-04-21 19:37:26 +0300

    seo keywords

--- src/@lekoarts/gatsby-theme-minimal-blog/components/homepage.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/homepage.tsx
@@ -1,49 +1,58 @@
 /** @jsx jsx */
-import { jsx } from "theme-ui"
-import { Link } from "gatsby"
-import Layout from '@lekoarts/gatsby-theme-minimal-blog/src/components/layout';
+import { jsx } from "theme-ui";
+import { Link } from "gatsby";
+import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout";
 // @ts-ignore
-import Hero from '@lekoarts/gatsby-theme-minimal-blog/src/texts/hero';
+import Hero from "@lekoarts/gatsby-theme-minimal-blog/src/texts/hero";
 // @ts-ignore
-import Bottom from '@lekoarts/gatsby-theme-minimal-blog/src/texts/bottom';
-import Title from '@lekoarts/gatsby-theme-minimal-blog/src/components/title';
-import Listing from '@lekoarts/gatsby-theme-minimal-blog/src/components/listing';
-import List from '@lekoarts/gatsby-theme-minimal-blog/src/components/list';
-import useMinimalBlogConfig from '@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-minimal-blog-config';
-import replaceSlashes from '@lekoarts/gatsby-theme-minimal-blog/src/utils/replaceSlashes';
+import Bottom from "@lekoarts/gatsby-theme-minimal-blog/src/texts/bottom";
+import Title from "@lekoarts/gatsby-theme-minimal-blog/src/components/title";
+import Listing from "@lekoarts/gatsby-theme-minimal-blog/src/components/listing";
+import List from "@lekoarts/gatsby-theme-minimal-blog/src/components/list";
+import useMinimalBlogConfig from "@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-minimal-blog-config";
+import replaceSlashes from "@lekoarts/gatsby-theme-minimal-blog/src/utils/replaceSlashes";
+import { Helmet } from "react-helmet";
 
 type PostsProps = {
   posts: {
-    slug: string
-    title: string
-    date: string
-    excerpt: string
-    description: string
-    timeToRead?: number
+    slug: string;
+    title: string;
+    date: string;
+    excerpt: string;
+    description: string;
+    timeToRead?: number;
     tags?: {
-      name: string
-      slug: string
-    }[]
-  }[]
-}
+      name: string;
+      slug: string;
+    }[];
+  }[];
+};
 
 const Homepage = ({ posts }: PostsProps) => {
-  const { basePath, blogPath } = useMinimalBlogConfig()
+  const { basePath, blogPath } = useMinimalBlogConfig();
 
   return (
     <Layout>
+      <Helmet>
+        <meta
+          name="keywords"
+          content="Joe Ng'ethe, Joe Ngethe, Joey Ng'ethe, Joey Ngethe, Joey, Ngethe, Ng'ethe, Frontend Engineer in Kenya, Frontend Engineer, Software Engineer, Engineers in Kenya, Nairobi Software Engineers, Graphql, React, Gatsby, Nextjs, Prismajs, javascript, frontend, css, UX engineers, Nairobi Graphql"
+        />
+      </Helmet>
       <section sx={{ mb: [4, 4, 6], p: { fontSize: [1, 2, 3], mt: 2 } }}>
         <Hero />
       </section>
       <Title text="Latest Posts">
-        <Link to={replaceSlashes(`/${basePath}/${blogPath}`)}>Read all posts</Link>
+        <Link to={replaceSlashes(`/${basePath}/${blogPath}`)}>
+          Read all posts
+        </Link>
       </Title>
       <Listing posts={posts} showTags={false} />
       <List>
         <Bottom />
       </List>
     </Layout>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
